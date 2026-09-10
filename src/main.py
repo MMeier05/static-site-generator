be53117438062b8f0e1388b12665106b64e1b9e0
MMeier05 <maximilian3305@outlook.de>
2026-09-10 16:46:59 +0200

    Now able to generate pages

--- src/main.py
+++ src/main.py
@@ -1,8 +1,67 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+from markdown_blocks import markdown_to_html_node
 
 def main():
-    node = TextNode("some text", TextType.ITALIC, "some url")
-    print(node)
+    copy_static_public()
+    generate_pages_recursive("content/", "template.html", "public/")
+
+def copy_static_public():
+    if os.path.exists("public/"):
+        shutil.rmtree("public/")
+        os.mkdir("public/")
+    else:
+        os.mkdir("public/")
+    recursive_helper()
+
+
+def recursive_helper(path="static/"):
+    entries = os.listdir(path)
+    for entry in entries:
+        dest = os.path.join("public/", path.removeprefix("static/"), entry)
+        source = os.path.join(path, entry)
+        if os.path.isfile(source):
+            print("FILE: ", source, "TO: ", dest)
+            shutil.copy(source, dest)
+        else:
+            print("DIR: ", source, "TO: ", dest)
+            os.mkdir(dest)
+            recursive_helper(source)
+
+
+def extract_title(markdown):
+    lines_list = markdown.split("\n")
+    for line in lines_list:
+        if line.startswith("# "):
+            return line.removeprefix("# ")
+    raise Exception("No header found")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}") 
+    f = open(from_path, 'rt')
+    md_file = f.read()
+    f.close()
+    f = open(template_path, 'rt')
+    template = f.read()
+    f.close()
+    nodes = markdown_to_html_node(md_file)
+    html = nodes.to_html()
+    title = extract_title(md_file)
+    template_with_title = template.replace("{{ Title }}", title)
+    template_title_content = template_with_title.replace("{{ Content }}", html)
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    f = open(dest_path, 'w')
+    f.write(template_title_content)
+    f.close()
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for item in os.listdir(dir_path_content):
+        src_path = os.path.join(dir_path_content, item)
+        dst_path = os.path.join(dest_dir_path, item)
+        if os.path.isfile(src_path):
+            generate_page(src_path, template_path, os.path.join(dest_dir_path, "index.html"))
+        else:
+            generate_pages_recursive(src_path, template_path, dst_path)
 
 if __name__ == "__main__":
     main()
---
+++ src/test_main.py
@@ -0,0 +1,23 @@
+import unittest
+from main import extract_title
+
+class TestMain(unittest.TestCase):
+
+    def test_extract_title(self):
+        md = """# Tolkien Fan Club
+
+![JRR Tolkien sitting](/images/tolkien.png)
+
+Here's the deal, **I like Tolkien**.
+
+> "I am in fact a Hobbit in all but size."
+>
+> -- J.R.R. Tolkien
+
+## Blog posts
+"""
+        title = extract_title(md)
+        self.assertEqual(title, "Tolkien Fan Club")
+
+if __name__ == '__main__':
+    unittest.main()
